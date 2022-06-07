d1c4dbd79964e53272cba904b9c530c1cd582307
Yogarani B <yogarani2014@gmail.com>
2022-06-07 15:45:09 +0530

    submit

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
+  loginform:any =FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.loginform = new FormGroup({
+      username : new FormControl(null,[ Validators.minLength(5),Validators.required]),
+      password : new FormControl(null,[ Validators.minLength(5),Validators.required]),
+    })
   }
+  submit(){
+if(this.loginform.valid)
+console.log(this.loginform.value);
+  }
+  onSubmit(){
 
+  }
 }
